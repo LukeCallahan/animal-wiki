7b7b351b403bf8cd69baaf6f3f0a5a1d2de2bc4a
LukeCallahan <lukemcal@gmail.com>
2022-10-19 13:45:14 -0700

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,3 @@
+// Make all divs hidden by default
+// when the user types in turtle, for example, remove the hidden attribute from class.
+// include an else to handle incorrect form inputs. Show a message to user that they've made an error.
